95cbd3ac10947c2e48270d59e65aceb645559595
Avital <avital@gmail.com>
2026-02-23 10:41:45 +0200

    Initial DevOps homework

---
+++ src/main.py
@@ -0,0 +1,7 @@
+def calculate_square():
+    result = 2 * 2
+    print(f"Hello! 2 * 2 = {result}")
+    return result
+
+if __name__ == "__main__":
+    calculate_square()
---
+++ test_script.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+# הוספת נתיב התיקייה כדי שנוכל לייבא את main
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.main import calculate_square
+
+def test_logic():
+    assert calculate_square() == 4
+    print("Test Passed!")
+
+if __name__ == "__main__":
+    test_logic()
